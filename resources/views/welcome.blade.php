26e43ac831e86c19ce5755480b5657e17993babd
Jccalix <jccalix6@gmail.com>
2024-12-17 15:23:01 -0600

    Update layout structure and enhance post views with Blade components; upgrade PostCSS and TailwindCSS dependencies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Hello World</h1>
-    <form action="/1" method="POST">
-        @csrf
-        <input type="text" name="username" id="">
-        <button type="submit">Submit</button>
-    </form>
-    <form action="/s" method="POST">
-        @csrf
-       @method('PUT')
-        <input type="text" name="username" id="">
-        <button type="submit">Submit Put</button>
-    </form> 
-    <form action="/s" method="POST">
-        @csrf
-       @method('delete')
-        <button type="submit">Submit Delete</button>
-    </form>
-</body>
-
-</html>
+<x-layout>
+    <h1>Welcome</h1>
+    <p>Test</p>
+    </div>
+</x-layout>
